5886f27d0291e2b54084b705e50e96c2ec713be0
Fausto Márquez <fausto_7m@hotmail.com>
2024-07-20 15:28:26 -0300

    django-tailwind finally watching changes on template files

--- backend/settings.py
+++ backend/settings.py
@@ -154,7 +154,10 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticfiles"),
+    os.path.join(BASE_DIR, "theme", "static"),
+]
 STATICFILES_FINDERS = [
     # First add the two default Finders, since this will overwrite the default.
     "django.contrib.staticfiles.finders.FileSystemFinder",
