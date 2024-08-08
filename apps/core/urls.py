78ff5a469a252ca6944ccf1ff784a74fbc7a446e
Fausto Márquez <fausto_7m@hotmail.com>
2024-08-08 01:36:59 -0300

    WIP vendors sidebar

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -5,7 +5,7 @@ app_name = "core"
 
 urlpatterns = [
     path("", views.landing_page, name="landing_page"),
-    path("login", views.login_view, name="login"),
+    path("login/", views.login_view, name="login"),
     path("error/", views.error, name="error"),
     path("contact/", views.contact, name="contact"),
     path(
